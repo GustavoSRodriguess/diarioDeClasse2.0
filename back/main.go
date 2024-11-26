f991ca161beb951b36b6768314f9cde836ea9f0e
Fhuller <fhullerbr@gmail.com>
2024-11-25 23:26:24 -0300

    add env file and gitignore

--- back/main.go
+++ back/main.go
@@ -4,15 +4,33 @@ import (
 	"cadastro-professores/models"
 	"cadastro-professores/routes"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// Configurar as credenciais de conexão com o PostgreSQL
-	dsn := "sua url"
+	// Carregar as variáveis de ambiente do arquivo .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+
+	// Construir a string DSN com base nas variáveis de ambiente
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_TIMEZONE"))
+
+	// Conectar ao banco de dados
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
